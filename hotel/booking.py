e7b581117174dfffd0b7885a402603a6be4ccb4a
adam krawiec <a.krawiec@exlabs.co.uk>
2024-03-17 00:34:11 +0100

    add modules and some tweaks to structure

--- hotel/booking.py
+++ hotel/booking.py
@@ -1,24 +1,3 @@
-from datetimerange import DateTimeRange
-from .booking_price_calculator import BookingPriceCalculator
-from .booking_repository import BookingRepository
-
-class Booking:
-    def __init__(self, room, start, end):
-        self.room = room
-        self.start = start
-        self.end = end
-        self.period = DateTimeRange(self.start, self.end)
-        self.id = None
-
-    def save(self):
-        self.id = BookingRepository().persist(self).id
-        return self
-
-    def price(self):
-        return BookingPriceCalculator(self.room.price_day, self.period).calculate()
-
-    def __str__(self):
-        return f"room: {self.room} - start: {self.start} end: {self.end}"
-
-    def __repr__(self):
-        return f"booking<id: {self.id} room: {self.room} start: {self.start} end: {self.end}>"
+from .bookings.booking_price_calculator import BookingPriceCalculator
+from .bookings.booking_repository import BookingRepository
+from .bookings.booking import Booking
